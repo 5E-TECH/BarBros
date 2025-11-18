6e7c1c20c34ec7676dde6c1b8e5b5e97f5ebfbe1
Bahodir Nabijanov <bahodirnabijanov782@gmail.com>
2025-11-18 15:26:13 +0500

    enum

--- src/shared/enum/index.ts
+++ src/shared/enum/index.ts
@@ -6,13 +6,3 @@ export const UserRole = {
 } as const;
 
 export type UserRole = (typeof UserRole)[keyof typeof UserRole];
-
-
-// export const UserRole = {
-//   Foydalanuvchi: "foydalanuvchi",
-//   Admin: "admin",
-//   Sartarosh: "sartarosh",
-//   Sartaroshxona: "sartaroshxona",
-// } as const;
-
-// export type UserRole = (typeof UserRole)[keyof typeof UserRole];
