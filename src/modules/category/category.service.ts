efc9c575b09ae454019de4426d5793388913ff59
Zuxriddinov <zuxriddinovoff@gmail.com>
2025-12-19 19:03:24 +0500

    category ayol erkark turiga bo'linib va barbershoplar faqat o'zini barberlarini alohida get qilinadigon bo'ldi

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,12 +1,17 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoryEntitiy } from './entitiy/category.entitiy';
-import { ILike, Repository } from 'typeorm';
+import { ILike, Not, Repository } from 'typeorm';
 import { ErrorHender } from 'src/utils/catchError';
 import { CreateCategoryDto } from './dto/category.dto';
 import { successRes } from 'src/utils/succesResponse';
 import { ImageValidationPipe } from 'src/common/pipe/img-validation';
 import { FileService } from '../file/file.service';
+import { Category } from 'src/common/enum';
 
 @Injectable()
 export class CategoryService {
@@ -58,33 +63,82 @@ export class CategoryService {
     }
   }
 
-
-
-  async updateCategory(){
+  async getAllManCategory() {
     try {
-      
+      const category = await this.categoryRepo.find({
+        where: { categoryType: Category.MAN },
+      });
+      return successRes(category);
     } catch (error) {
-     return ErrorHender(error)
-      
+      return ErrorHender(error);
     }
   }
 
-
-  async deletCategory(id:number){
+  async getAllWomanCategory() {
     try {
-      const category = await this.categoryRepo.findOne({where:{id}})
-      if(!category) throw new NotFoundException("Not found category")
-
-        const categorys = await this.categoryRepo.remove(category)
-        return successRes(categorys)
-
+      const category = await this.categoryRepo.find({
+        where: { categoryType: Category.WOMAN },
+      });
+      return successRes(category);
     } catch (error) {
-      ErrorHender(error)
-      
+      return ErrorHender(error);
     }
   }
 
+  async updateCategory(
+    id: number,
+    updateCategoryDto: CreateCategoryDto,
+    file?: Express.Multer.File,
+  ) {
+    try {
+      const { name, categoryType } = updateCategoryDto;
 
+      const category = await this.categoryRepo.findOne({
+        where: { id },
+      });
 
-  
+      if (!category) {
+        throw new NotFoundException('Category not found');
+      }
+
+      const existCategory = await this.categoryRepo.findOne({
+        where: {
+          name: ILike(name),
+          categoryType: categoryType,
+          id: Not(id),
+        },
+      });
+
+      if (existCategory) {
+        throw new ConflictException(
+          'Category with this name and type already exists!',
+        );
+      }
+
+      category.name = name;
+      category.categoryType = categoryType;
+
+      if (file && new ImageValidationPipe().transform(file)) {
+        category.img = await this.fileServis.replaceFile(category.img, file);
+      }
+
+      await this.categoryRepo.save(category);
+
+      return successRes(category);
+    } catch (error) {
+      return ErrorHender(error);
+    }
+  }
+
+  async deletCategory(id: number) {
+    try {
+      const category = await this.categoryRepo.findOne({ where: { id } });
+      if (!category) throw new NotFoundException('Not found category');
+
+      const categorys = await this.categoryRepo.remove(category);
+      return successRes(categorys);
+    } catch (error) {
+      ErrorHender(error);
+    }
+  }
 }
